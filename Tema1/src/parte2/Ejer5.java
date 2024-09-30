37ae611c245b16de1950b738b1b08a29715a053c
crist <crist@LAPTOP-3BNPSUPH>
2024-09-30 17:47:24 +0200

    hh

---
+++ Tema1/src/parte2/Ejer5.java
@@ -0,0 +1,30 @@
+package parte2;
+
+import java.util.Scanner;
+
+public class Ejer5 {
+
+	public static void main(String[] args) {
+		// Introducimos las variables
+		int horas;
+		int minutos;
+		int segundos;
+		int segundosRestantes;
+		// Creamos el escáner
+		Scanner sc = new Scanner(System.in);
+		// Pedimos al usuario el número de segundos
+		System.out.println("Introduzca segundos");
+		// Leemos el dato
+		segundos = sc.nextInt();
+		// Hacemos los cálculos
+		horas = segundos / 3600;
+		minutos = (segundos % 3600) / 60;
+		segundosRestantes = (segundos % 3600) - minutos*60;
+		// Expresamos el resultado
+		System.out.println("Los " + segundos + " segundos que pusiste equivalen a " + horas + " horas, " + minutos
+				+ " minutos, y " + segundosRestantes + " segundos");
+		sc.close();
+
+	}
+
+}
