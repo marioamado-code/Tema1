c9494f4a49fcf6c1b7105c0ee7442544cb50c460
mario.amado <mario.amado@106-04.info2>
2024-09-30 13:45:02 +0200

    mmm

--- Tema1/src/parte1/Ejer6.java
+++ Tema1/src/parte1/Ejer6.java
@@ -6,16 +6,16 @@ public class Ejer6 {
 
 	public static void main(String[] args) {
 		// Definimos las variables
-				int num1;
-				int num2;
-				//Introducimos las variables de resultados
-				int suma;
-				int resta;
-				int división;
-				int multiplicación;
+		int num1;
+		int num2;
+		// Introducimos las variables de resultados
+		int suma;
+		int resta;
+		int división;
+		int multiplicación;
 		// Creamos el escáner
 		Scanner sc = new Scanner(System.in);
-		
+
 		// Pedimos introducir el primer número
 		System.out.println("Introduzca primer número");
 		// Metemos ese número en consola
@@ -24,20 +24,19 @@ public class Ejer6 {
 		System.out.println("Introduzca segundo número");
 		// Metemos ese número en consola
 		num2 = sc.nextInt();
-		
-		//Hacemos el cálculo
-		suma = num1+num2;
-		resta = num1-num2;
-		división = num1/num2;
-		multiplicación = num1*num2;
-		//Expresamos los resultados
+
+		// Hacemos el cálculo
+		suma = num1 + num2;
+		resta = num1 - num2;
+		división = num1 / num2;
+		multiplicación = num1 * num2;
+		// Expresamos los resultados
 		System.out.println("La suma da " + suma);
 		System.out.println("La resta da " + resta);
 		System.out.println("La multiplicación da " + multiplicación);
 		System.out.println("La división da " + división);
-		//Cerramos el escáner
+		// Cerramos el escáner
 		sc.close();
-		
 
 	}
 
---
+++ Tema1/src/parte2/Ejer2.java
@@ -0,0 +1,25 @@
+package parte2;
+
+import java.util.Scanner;
+
+public class Ejer2 {
+
+	public static void main(String[] args) {
+		// Introducimos la variable
+		int num;
+		int contador = 0;
+
+		// Creamos el escáner
+		Scanner sc = new Scanner(System.in);
+		// Pedimos al usuario el número
+		System.out.println("Introduzca número ");
+		// Leemos el número
+		num = sc.nextInt();
+		//Vemos si es múltiplo de 7
+        num%7 = 0;
+	}
+			
+		}
+	
+
+
