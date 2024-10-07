0384f044726bd74f64730d6e7af635131bda04cd
crist <crist@LAPTOP-3BNPSUPH>
2024-10-07 19:58:58 +0200

    ejercicios 1 a 8

--- Tema1/src/parte2copia/Ejercicio1.java
+++ Tema1/src/parte2copia/Ejercicio1.java
@@ -1,5 +1,26 @@
 package parte2copia;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// Introducimos variable
+		float num;
+		final double MITAD = 0.5;
+		// Creamos el escáner
+		Scanner sc = new Scanner(System.in);
+		// Pedimos al usuario el número
+		System.out.println("Introduzca número decimal");
+		// Leemos el número
+		num = sc.nextFloat();
+		// Ahora tenemos que redondearlo
+		num =(int) (num + MITAD);
+		// Mostramos el resultado
+		System.out.println("El número redondeado es:"+ (int)num);
+		//Cerramos el escáner
+		sc.close();
+
+	}
 
 }
--- Tema1/src/parte2copia/Ejercicio4.java
+++ Tema1/src/parte2copia/Ejercicio4.java
@@ -1,5 +1,36 @@
 package parte2copia;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
+
+	public static void main(String[] args) {
+	//Creamos las variables
+		float a;
+		float b;
+		float c;
+		float y;
+		float x;
+		//Creamos el escáner
+		Scanner sc = new Scanner(System.in);
+		//Pedimos los valores de a, b, c y x, y lo llemos 
+		System.out.println("Introduzca valores para la función y = ax^2 +bx +c");
+		System.out.println("a:");
+		a = sc.nextFloat();
+		System.out.println("b:");
+		b = sc.nextFloat();
+		System.out.println("c:");
+		c = sc.nextFloat();
+		System.out.println("x:");
+		x = sc.nextFloat();
+		//Hacemos el cálculo final
+		y = a*x*x +b*x+c;
+		//Mostramos el resultado
+		System.out.println("El resultado de la ecuación es " + y);
+		//Cerramos el escáner
+		sc.close();
+		
+
+	}
 
 }
